9988585cac35d6d5a4cd39ae7a314322dd190294
StellaNjambi02 <njambindungistella@gmail.com>
2024-07-03 12:56:13 +0300

    modified grade generator function

--- js/gradeGenerator.js
+++ js/gradeGenerator.js
@@ -1,26 +1,43 @@
 // Function to calculate grades
- function getGrade(marks){
+const getGrade=function(){
+
+    let invalidMark=true;
+    let marks;
+
+
+    while(invalidMark){ 
+        // prompt user for input and convert it to number data type
+    marks=(prompt("Kindly input your marks:"));
+        // Use if for edge conditions
+    if(marks!=null&& marks&&marks>=0 && marks<=100){ 
+        // convert input to number
+     marks=Number(marks);
+        invalidMark=false;
+        
+    }
+    else{alert("Kindly enter a valid mark.")}
+    
+}
+let statement;
     if(marks>79){
-        return 'Your grade is A';
+        statement= 'Your grade is A';
     }
     else if (marks>=60 && marks <=79){
-        return 'Your grade is B';
+        statement= 'Your grade is B';
     }
     else if (marks>49 && marks<=59){
-        return 'Your grade is C';
+        statement ='Your grade is C';
     }
     else if (marks>=40 && marks<=50){
-        return 'Your grade is D';
+        statement ='Your grade is D';
     }
     else{
-        return 'Your grade is E';
+        statement= 'Your grade is E';
     }
+    alert (statement)
 }
+getGrade();
 
-// prompt user for input and convert it to number data type
-    let marks=Number(prompt("Kindly input your marks:"));
-    // Use if statement and call function
-    if(marks>=0 && marks<=100){ 
-        getGrade(marks);
-    }
-    alert (getGrade(marks))
+
+   
+    
